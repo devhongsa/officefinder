26eb266412cddf569c7e79b629281e9369f2d78a
ForteEscape <sehun5515@naver.com>
2023-08-11 19:23:19 +0900

    chore: profile 테스트

--- src/test/java/com/dokkebi/officefinder/OfficefinderApplicationTests.java
+++ src/test/java/com/dokkebi/officefinder/OfficefinderApplicationTests.java
@@ -3,7 +3,7 @@ package com.dokkebi.officefinder;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = OfficefinderApplication.class)
 class OfficefinderApplicationTests {
 
   @Test
