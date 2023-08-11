98a3182af4267186a7e253e69c81e64db96846d7
ForteEscape <sehun5515@naver.com>
2023-08-11 13:02:11 +0900

    init project

---
+++ src/main/java/com/dokkebi/officefinder/OfficefinderApplication.java
@@ -0,0 +1,16 @@
+package com.dokkebi.officefinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Profile;
+
+@Profile("local")
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+public class OfficefinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OfficefinderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dokkebi/officefinder/OfficefinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dokkebi.officefinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OfficefinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
