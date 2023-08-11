ea2ed42cc92a55158cacd2e39895b9eff833c386
ForteEscape <sehun5515@naver.com>
2023-08-11 14:32:54 +0900

    chore: test container 사용을 위한 설정 코드 등록 및 테스트

---
+++ src/test/java/com/dokkebi/officefinder/testcontainer/TestContainerTest.java
@@ -0,0 +1,28 @@
+package com.dokkebi.officefinder.testcontainer;
+
+import com.dokkebi.officefinder.OfficefinderApplication;
+import com.dokkebi.officefinder.TestContainerConfig;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+
+@SpringBootTest
+public class TestContainerTest extends TestContainerConfig{
+
+  @Autowired
+  private RedisTemplate<String, String> redisTemplate;
+
+  @DisplayName("container test")
+  @Test
+  public void myTest() {
+    // given
+    redisTemplate.opsForValue().set("something", "something");
+    // when
+    String something = redisTemplate.opsForValue().getAndDelete("something");
+    // then
+    Assertions.assertThat(something).isEqualTo("something");
+  }
+}
