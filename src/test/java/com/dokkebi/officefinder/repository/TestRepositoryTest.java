54651def235fa5b99389fb557e6ae3070ba53384
ForteEscape <sehun5515@naver.com>
2023-08-12 03:01:43 +0900

    test: test repository test 구현

---
+++ src/test/java/com/dokkebi/officefinder/repository/TestRepositoryTest.java
@@ -0,0 +1,33 @@
+package com.dokkebi.officefinder.repository;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import com.dokkebi.officefinder.entity.TestEntity;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
+class TestRepositoryTest {
+
+  @Autowired
+  private TestRepository testRepository;
+
+  @DisplayName("repository Test")
+  @Test
+  public void repositoryTest() {
+    // given
+    TestEntity testEntity = TestEntity.builder()
+        .name("test name")
+        .build();
+
+    // when
+    TestEntity savedTest = testRepository.save(testEntity);
+    TestEntity result = testRepository.findById(savedTest.getId()).get();
+
+    // then
+    Assertions.assertThat(result.getName()).isEqualTo("test name");
+  }
+}
