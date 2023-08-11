0868654fe65b3345cd168a416f9b0b6df2e99a48
ForteEscape <sehun5515@naver.com>
2023-08-11 14:31:09 +0900

    chore: configuration 파일 등록 - redisTemplate - swagger - JpaAuditing - PasswordEncoder

---
+++ src/main/java/com/dokkebi/officefinder/config/RedisConfig.java
@@ -0,0 +1,66 @@
+package com.dokkebi.officefinder.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.CacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfig {
+
+  @Value("${REDIS_HOST:localhost}")
+  private String host;
+
+  @Value("${REDIS_PORT:6379}")
+  private int port;
+
+  @Bean
+  public RedisConnectionFactory redisConnectionFactory(){
+    RedisStandaloneConfiguration conf = new RedisStandaloneConfiguration();
+    conf.setHostName(host);
+    conf.setPort(port);
+    conf.setPassword("dev");
+
+    return new LettuceConnectionFactory(conf);
+  }
+
+  @Bean
+  public CacheManager redisCacheManager(RedisConnectionFactory redisConnectionFactory) {
+    RedisCacheConfiguration conf = RedisCacheConfiguration.defaultCacheConfig()
+        .serializeKeysWith(
+            RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer())
+        )
+        .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(
+                new GenericJackson2JsonRedisSerializer()
+            )
+        );
+
+    return RedisCacheManager.RedisCacheManagerBuilder
+        .fromConnectionFactory(redisConnectionFactory)
+        .cacheDefaults(conf)
+        .build();
+  }
+
+  @Bean
+  public RedisTemplate<String, ?> redisTemplate(){
+    RedisTemplate<String, ?> redisTemplate = new RedisTemplate<>();
+    redisTemplate.setConnectionFactory(redisConnectionFactory());
+
+    redisTemplate.setKeySerializer(new StringRedisSerializer());
+    redisTemplate.setValueSerializer(new Jackson2JsonRedisSerializer<>(String.class));
+    redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+    redisTemplate.setHashValueSerializer(new StringRedisSerializer());
+
+    return redisTemplate;
+  }
+}
---
+++ src/main/java/com/dokkebi/officefinder/config/SwaggerConfig.java
@@ -0,0 +1,90 @@
+package com.dokkebi.officefinder.config;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.data.domain.Pageable;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.Contact;
+import springfox.documentation.service.SecurityReference;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Profile(value = {"local", "release"})
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+  @Bean
+  public Docket api() {
+    return new Docket(DocumentationType.SWAGGER_2)
+        .securitySchemes(Collections.singletonList(apiKey()))
+        .securityContexts(Collections.singletonList(securityContext()))
+        .ignoredParameterTypes(Pageable.class)
+        .consumes(getConsumeContentTypes())
+        .produces(getProduceContentType())
+        .apiInfo(getApiInfo())
+        .select()
+        .apis(RequestHandlerSelectors.basePackage("com.dokkebi.officefinder"))
+        .paths(PathSelectors.ant("/**"))
+        .build();
+  }
+
+  private ApiKey apiKey(){
+    return new ApiKey("apiKey", "Authorization", "header");
+  }
+
+  // request content type setting
+  private Set<String> getConsumeContentTypes() {
+    Set<String> consumes = new HashSet<>();
+    consumes.add("application/json;charset=UTF-8");
+
+    return consumes;
+  }
+
+  // response content type setting
+  private Set<String> getProduceContentType() {
+    Set<String> produces = new HashSet<>();
+    produces.add("application/json;charset=UTF-8");
+
+    return produces;
+  }
+
+  private ApiInfo getApiInfo() {
+    return new ApiInfoBuilder()
+        .title("API")
+        .description("Office-Finder API Document")
+        .contact(
+            new Contact("OfficeFinderTeam", "https://github.com/ForteEscape/officefinder",
+                "")
+        )
+        .version("1.0")
+        .build();
+  }
+
+  private SecurityContext securityContext() {
+    return springfox.documentation.spi.service.contexts.SecurityContext
+        .builder()
+        .securityReferences(securityReference()).forPaths(PathSelectors.any())
+        .build();
+  }
+
+  private List<SecurityReference> securityReference(){
+    AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+    AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+    authorizationScopes[0] = authorizationScope;
+
+    return Collections.singletonList(new SecurityReference("apiKey", authorizationScopes));
+  }
+}
