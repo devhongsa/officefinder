c80564edf8b4ab4322a45e505040731bd5acf2e1
ForteEscape <sehun5515@naver.com>
2023-08-15 17:18:20 +0900

    Feat: 오피스 생성 및 수정 DTO 구현

---
+++ src/main/java/com/dokkebi/officefinder/controller/office/dto/OfficeCreateRequestDto.java
@@ -0,0 +1,49 @@
+package com.dokkebi.officefinder.controller.office.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class OfficeCreateRequestDto {
+
+  // address
+  String officeName;
+  String legion;
+  String city;
+  String town;
+  String village;
+  String bungi;
+  String street;
+  String buildingNumber;
+  Integer zipcode;
+
+  Integer maxCapacity;
+  Long leaseFee;
+
+  Double latitude;
+  Double longitude;
+
+  // option
+  Boolean haveAirCondition;
+  Boolean haveCafe;
+  Boolean havePrinter;
+  Boolean packageSendServiceAvailable;
+  Boolean haveDoorLock;
+  Boolean faxServiceAvailable;
+  Boolean havePublicKitchen;
+  Boolean havePublicLounge;
+  Boolean havePrivateLocker;
+  Boolean haveTvProjector;
+  Boolean haveWhiteBoard;
+  Boolean haveWifi;
+  Boolean haveShowerBooth;
+  Boolean haveStorage;
+  Boolean haveHeater;
+}
