f25dd10acd65cfb3b37325678f45324d1e3a88aa
ForteEscape <sehun5515@naver.com>
2023-08-16 16:47:04 +0900

    Feat: 입력 DTO Validation 적용

--- src/main/java/com/dokkebi/officefinder/controller/office/dto/OfficeModifyRequestDto.java
+++ src/main/java/com/dokkebi/officefinder/controller/office/dto/OfficeModifyRequestDto.java
@@ -1,5 +1,7 @@
 package com.dokkebi.officefinder.controller.office.dto;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -13,37 +15,66 @@ import lombok.Setter;
 @Builder
 public class OfficeModifyRequestDto {
 
+  @NotEmpty
   String officeName;
+  @NotNull
   Integer maxCapacity;
+  @NotNull
   Long leaseFee;
+  @NotNull
   Integer remainRoom;
 
   // address
+  @NotEmpty
   String legion;
+  @NotEmpty
   String city;
+  @NotEmpty
   String town;
+  @NotNull
   String village;
+  @NotEmpty
   String bungi;
+  @NotEmpty
   String street;
+  @NotEmpty
   String buildingNumber;
+  @NotNull
   Integer zipcode;
+  @NotNull
   Double latitude;
+  @NotNull
   Double longitude;
 
   // option
+  @NotNull
   Boolean haveAirCondition;
+  @NotNull
   Boolean haveCafe;
+  @NotNull
   Boolean havePrinter;
+  @NotNull
   Boolean packageSendServiceAvailable;
+  @NotNull
   Boolean haveDoorLock;
+  @NotNull
   Boolean faxServiceAvailable;
+  @NotNull
   Boolean havePublicKitchen;
+  @NotNull
   Boolean havePublicLounge;
+  @NotNull
   Boolean havePrivateLocker;
+  @NotNull
   Boolean haveTvProjector;
+  @NotNull
   Boolean haveWhiteBoard;
+  @NotNull
   Boolean haveWifi;
+  @NotNull
   Boolean haveShowerBooth;
+  @NotNull
   Boolean haveStorage;
+  @NotNull
   Boolean haveHeater;
 }
