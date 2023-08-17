d429966879df4c268f2bf1edd1dd618037b5ca3c
Sanghun77 <tpspfkdl@naver.com>
2023-08-17 14:42:36 +0900

    feature : 리뷰 작성

--- src/main/java/com/dokkebi/officefinder/repository/LeaseRepository.java
+++ src/main/java/com/dokkebi/officefinder/repository/LeaseRepository.java
@@ -1,8 +1,12 @@
 package com.dokkebi.officefinder.repository;
 
 import com.dokkebi.officefinder.entity.lease.Lease;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface LeaseRepository extends JpaRepository<Lease, Long> {
 
+  @Query("select l from Lease l join fetch l.customer join fetch l.office where l.id = :leaseId")
+  Optional<Lease> findByLeaseId(Long leaseId);
 }
