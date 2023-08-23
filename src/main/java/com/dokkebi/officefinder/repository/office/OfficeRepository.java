290f457d235ff9a78bd2931dabf74fac401cfe79
Aaron <dnrwhd3@naver.com>
2023-08-23 16:40:59 +0900

    feat : 특정 오피스에 대한 임대 요청 리스트 조회 기능 구현
    로직은 다음과 같이 동작합니다.
    - officeId와 회원 정보를 바탕으로 Office와 OfficeOwner를 조회합니다.
    - 수락 대기 상태(LeaseStatus.AWAIT)에 있는 임대 요청 리스트를 리턴합니다.
    - 조회 후 임대업자는 임대 요청을 수락 또는 거절할 수 있습니다.(추후 바로 구현)

--- src/main/java/com/dokkebi/officefinder/repository/office/OfficeRepository.java
+++ src/main/java/com/dokkebi/officefinder/repository/office/OfficeRepository.java
@@ -1,8 +1,10 @@
 package com.dokkebi.officefinder.repository.office;
 
+import com.dokkebi.officefinder.entity.OfficeOwner;
 import com.dokkebi.officefinder.entity.office.Office;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface OfficeRepository extends JpaRepository<Office, Long>, OfficeRepositoryCustom {
-
+  Optional<Office> findByOwnerAndId(OfficeOwner officeOwner, Long officeId);
 }
