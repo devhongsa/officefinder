4a3c438ca3365059325f592d4179dfdf03d669c1
Aaron <dnrwhd3@naver.com>
2023-08-23 16:35:48 +0900

    feat : officeId + LeaseStatus(AWAIT)를 통해 임대 요청 리스트를 조회하는 메서드 구현

--- src/main/java/com/dokkebi/officefinder/repository/lease/LeaseRepositoryCustom.java
+++ src/main/java/com/dokkebi/officefinder/repository/lease/LeaseRepositoryCustom.java
@@ -1,10 +1,14 @@
 package com.dokkebi.officefinder.repository.lease;
 
 import com.dokkebi.officefinder.entity.lease.Lease;
+import com.dokkebi.officefinder.entity.office.Office;
+import com.dokkebi.officefinder.entity.type.LeaseStatus;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 public interface LeaseRepositoryCustom {
 
   Page<Lease> findByCustomerId(Long customerId, Pageable pageable);
+
+  Page<Lease> findByOfficeIdAndLeaseStatus(Long officeId, LeaseStatus leaseStatus,Pageable pageable);
 }
