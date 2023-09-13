ac098fa6aa8400aea7c9e54994424546a8096634
ForteEscape <sehun5515@naver.com>
2023-09-13 22:56:17 +0900

    Refactor: string으로 받은 뒤 long 으로 parse하도록 변경

--- src/main/java/com/dokkebi/officefinder/controller/bookmark/dto/SubmitDto.java
+++ src/main/java/com/dokkebi/officefinder/controller/bookmark/dto/SubmitDto.java
@@ -1,13 +1,20 @@
 package com.dokkebi.officefinder.controller.bookmark.dto;
 
+import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
+@Setter
 @AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class SubmitDto {
 
-  @Positive
-  private Long officeId;
+  @NotEmpty
+  private String officeId;
 }
